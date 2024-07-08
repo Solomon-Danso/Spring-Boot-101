8f1f220ad7f39b32a98c7f6618bc14fb6b3d0d13
Solomon <solomondanso2023@gmail.com>
2024-07-08 12:39:58 +0000

    Student Class Added

--- src/main/java/com/example/hydottech/learnspringbootvideo/DemoApplication.java
+++ src/main/java/com/example/hydottech/learnspringbootvideo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.hydottech.learnspringbootvideo;
 
+import com.example.hydottech.learnspringbootvideo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 
 @SpringBootApplication
@@ -15,9 +19,27 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/")
-	public List<String> GetHello(){
+	public List<Student> GetHello(){
 
-		return List.of("First","Hello Advance Money and Code");
+		return List.of(
+				new Student(
+						1L,
+						"Solomon Danso",
+						30,
+						LocalDate.of(1999, Month.DECEMBER, 31),
+						"solomondanso@gmail.com"
+
+				),
+				new Student(
+						2L,
+						"Isaac Frimpong",
+						20,
+						LocalDate.of(2012, Month.DECEMBER, 31),
+						"isaacfrimpong@gmail.com"
+
+				)
+
+		);
 	}
 
 }
--- src/main/java/com/example/hydottech/learnspringbootvideo/Student/Student.java
+++ src/main/java/com/example/hydottech/learnspringbootvideo/Student/Student.java
@@ -1,4 +1,89 @@
 package com.example.hydottech.learnspringbootvideo.Student;
 
+import java.time.LocalDate;
+
 public class Student {
+
+
+    private Long Id;
+    private String Name;
+    private Integer Age;
+    private LocalDate DOB;
+    private String Email;
+
+    public Student(Long id, String name, Integer age, LocalDate DOB, String email) {
+        Id = id;
+        Name = name;
+        Age = age;
+        this.DOB = DOB;
+        Email = email;
+    }
+
+
+    public Student(String name, Integer age, LocalDate DOB, String email) {
+        Name = name;
+        Age = age;
+        this.DOB = DOB;
+        Email = email;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+    public void setAge(Integer age) {
+        Age = age;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", Name='" + Name + '\'' +
+                ", Age=" + Age +
+                ", DOB=" + DOB +
+                ", Email='" + Email + '\'' +
+                '}';
+    }
+
+
+
+
+
+
+
+
+
 }
